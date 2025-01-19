ac24a4bff709603170b129523b30dd1de5ae47c6
unknown <kapilojha@outlook.com>
2025-01-19 13:49:43 +0545

    first commit

---
+++ Manager/view.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse,HttpResponseRedirect
+from django.shortcuts import render,redirect
+
+def home(request):
+    data = {
+        'title':'Home Page New',
+        'body1':'this is made by using directories',
+        'klist': [ 'cat' , 'dog' ,'cow']
+            
+            }
+    return render(request,"index.html",data)
+
+def hello(request,hello):
+    return HttpResponse(hello)
+
+def form(request):
+    op=0
+    try:
+        if request.method == 'POST':
+        
+            a=int(request.POST['n1'])
+            b= int(request.POST['n2'])
+        
+            op = a+b
+            # return HttpResponseRedirect('/form') #this is used for redirect by using django.http
+            # return redirect('/') #this is used for redirect by using django.shortcut
+    except:
+        print('not calculate')
+    return render(request,'form.html', {'output' : op } )
+
+def sumit(request):
+    return render(request,sumit.html) 
